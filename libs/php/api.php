5c650e1815c2604ad40402d386b2421caf15f5f8
TamukaB <128832647+TamukaB@users.noreply.github.com>
2024-01-02 23:09:11 +0000

    Update api.php

--- libs/php/api.php
+++ libs/php/api.php
@@ -6,41 +6,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     include_once("../includes/db.php");
 
-    if ($api_type == 1) {
+    if ($api_type == 2)
+    {
         try {
-            $sql = "select p.*, d.name as department, l.name as location from personnel p inner join department d on p.departmentID = d.id inner join location l on d.locationID = l.id";
-            $result = mysqli_query($db_config, $sql);
-            if ($result) {
-                $data = array();
-                while ($row = mysqli_fetch_assoc($result)) {
-                    $data[] = $row;
-                }
-                echo json_encode($data);
-            } else {
-                echo json_encode(array('message' => 'No data found'));
-            }
-        } catch (PDOException $e) {
-            echo "Connection failed: " . $e->getMessage();
-        }
-    } else if ($api_type == 2) {
-        try {
-            $sql = "select * from location";
-            $result = mysqli_query($db_config, $sql);
-            if ($result) {
-                $data = array();
-                while ($row = mysqli_fetch_assoc($result)) {
-                    $data[] = $row;
-                }
-                echo json_encode($data);
-            } else {
-                echo json_encode(array('message' => 'No data found'));
-            }
-        } catch (PDOException $e) {
-            echo "Connection failed: " . $e->getMessage();
-        }
-    } else if ($api_type == 3) {
-        try {
-            $sql = "select d.id, d.name as department, l.name as location from department d inner join location l on d.locationID = l.id";
+            $sql = "select l.id, l.name, (SELECT COUNT(*) FROM department d WHERE d.locationID = l.id) AS deptCount from  location l order by l.name ASC";
             $result = mysqli_query($db_config, $sql);
             if ($result) {
                 $data = array();
